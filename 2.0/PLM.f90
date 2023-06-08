fd95e5b2932e4bae51a72e348ec438e82d4681f7
atulekey <atulekey@ucsc.edu>
2023-06-07 22:16:50 -0700

    start plm

---
+++ 2.0/PLM.f90
@@ -0,0 +1,15 @@
+module PLM
+use utility
+
+implicit none
+
+contains
+    subroutine PLM_updater()
+    use utility
+    implicit none
+
+
+
+    end subroutine PLM_updater
+
+end module PLM
---
+++ 2.0/plotter.m
@@ -0,0 +1,26 @@
+clear all; close all
+
+data = importdata('output_FOG.dat');
+
+x = linspace(0,1,length(data(1,2:end)));
+
+figure(1)
+plot(x,data(1,2:end))
+hold on
+grid minor
+plot(x,data(end,2:end))
+hold off
+
+%figure(2)
+ %   for t = 1:length(data(:,1))
+  %      u = data(t,2:end);
+   %     plot(x,u,'linewidth',1.5)
+        %xlim([0,1])
+        %ylim([-1.1,1.1])
+    %    grid minor
+     %  pause(2.0)
+
+        %if t == 12
+        %    break 
+        %end
+    %end
